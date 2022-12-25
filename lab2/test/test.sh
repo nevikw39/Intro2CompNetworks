8ca199fc900d741c655ccf36f57fcaad63d04145
nevikw39 <nevikw39@icloud.com>
2022-12-25 23:29:55 +0800

    Enhance Lab 3 test

--- lab2/test/test.sh
+++ lab2/test/test.sh
@@ -1,17 +1,25 @@
 cp test/video.mp4 bin
+cp include/lab2.h bin
 cd bin
 
+killall server
+
 ./server 9999 &
+sleep 0.1
 time ./client <<EOF
 127.0.0.1
 9999
 download video.mp4
+download video.mp4
+download lab2.h
+download server
+download client
 exit
 EOF
 kill $!
 
-r=$(diff video.mp4 download_video.mp4)
-rm *.mp4
+r=$(diff video.mp4 download_video.mp4 && diff lab2.h download_lab2.h)
+rm *.mp4 *.h download*
 
 if $r; then
     printf "\e[32mAC!!\e[0m\n"
